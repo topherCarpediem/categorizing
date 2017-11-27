57aa0c6073af6a013ae10fcf13c18932660e69df
Christopher Enriquez <topher.carpediem@gmail.com>
2017-11-27 17:32:27 +0800

    curl-less function

--- app/categorizing.py
+++ app/categorizing.py
@@ -6,7 +6,6 @@ from flask import request, abort
 
 @app.route('/', methods=['POST'])
 def hey():
-    if not request.json:
-        abort(400)
-    print(request.get_json())
+
+    print(request.form)
     return json.dumps(request.get_json())
---
+++ curl.php
@@ -0,0 +1,55 @@
+<?php
+// $url = 'http://127.0.0.1:5000/';
+// $data = array('key1' => 'value1', 'key2' => 'value2');
+
+// // use key 'http' even if you send the request to https://...
+// $options = array(
+//     'http' => array(
+//         'header'  => "Content-type: application/json\r\n",
+//         'method'  => 'POST',
+//         'content' => http_build_query($data)
+//     )
+// );
+// $context  = stream_context_create($options);
+// $result = file_get_contents($url,FALSE, $context);
+// if ($result === FALSE) { /* Handle error */ }
+
+// var_dump($result);
+
+// set post fields
+// $post = [
+//     'username' => 'user1',
+//     'password' => 'passuser1',
+//     'gender'   => 1,
+// ];
+
+// $ch = curl_init('http://127.0.0.1:5000');
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+// // execute!
+// $response = curl_exec($ch);
+
+// // close the connection, release resources used
+// curl_close($ch);
+
+// // do anything you want with your response
+// var_dump($response);
+
+$url = 'http://127.0.0.1:5000';
+$data = array('key1' => 'value1', 'key2' => 'value2');
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    ),
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+var_dump($result);
+
+?>
